74903963d8356e4afc21a013b1da5a70024ffee1
iwan <1322677864@qq.com>
2023-07-04 00:01:24 +0800

    fix

--- card-admin/vite.config.ts
+++ card-admin/vite.config.ts
@@ -1,36 +1,47 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+
+const crossOriginIsolation = () => ({
+  name: "configure-server",
+  configureServer(server) {
+    server.middlewares.use((_req, res, next) => {
+      res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+      res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+      next();
+    });
+  },
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), crossOriginIsolation()],
   // 路径别名配置
-  resolve:{
-    alias:{
-      "@":path.join(__dirname,"src")
-    }
+  resolve: {
+    alias: {
+      "@": path.join(__dirname, "src"),
+    },
   },
   // scss配置
   css: {
     postcss: {
-      plugins: [require("tailwindcss"), require("autoprefixer")]
+      plugins: [require("tailwindcss"), require("autoprefixer")],
     },
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "@/styles/variables.scss";`
+        additionalData: `@import "@/styles/variables.scss";`,
       },
-    }
+    },
   },
   // proxy代理跨域
   server: {
     proxy: {
       // 选项写法
-      '/api': {
-        target: 'http://localhost:5000/api',
+      "/api": {
+        target: "http://localhost:5000/api",
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api/, '')  //重写请求地址
-      }
-    }
-  }
-})
+      },
+    },
+  },
+});
